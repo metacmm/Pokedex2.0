88144d3740b05b697c784225f303dfcbd464cfa2
msheffield <msheffield@adaptivebiotech.com>
2019-10-12 13:54:36 -0700

    bar graph added, known bug - multiple charts on multiple searches. Need to add .destroy somewhere

--- assets/javascript/displayPokemon.js
+++ assets/javascript/displayPokemon.js
@@ -1,18 +1,55 @@
 function displayPokemon(pokemon) {
-    console.log(pokemon);
+    //console.log(pokemon);
     // add code to update UI elements here
 
-    pokemon.stats.forEach(element => {
-        let name = element.stat["name"];
-        let value = element["base_stat"];
-
-        $("#" + name).attr("aria-valuenow", value);
-        $("#" + name).attr("style", "width: " + ((parseInt(value) / 255) * 100) + "%");
-        $("#" + name).text(value);
-    })
+    updateStats(pokemon);
 
     var pokimage = $("<img class='mx-auto border border-dark'>");
     pokimage.attr("src", pokemon.sprites.front_default);
     $("#sprite").empty();
     $("#sprite").prepend(pokimage);
+}
+
+function updateStats(pokemon) {
+
+    var data = [];
+    var labels = [];
+
+    pokemon.stats.forEach(element => {
+        let name = element.stat["name"];
+        labels.push(name);
+
+        let value = element["base_stat"];
+        data.push(value);
+    })
+
+    var statData = {
+        datasets: [{
+            data: data,
+            backgroundColor: [
+                "#FF6384",
+                "#4BC0C0",
+                "#FFCE56",
+                "#E7E9ED",
+                "#36A2EB"
+            ],
+            label: 'Pokemon Stats' // for legend
+        }],
+        labels: labels,
+    };
+    var ctx = $("#statChart");
+
+    new Chart(ctx, {
+        data: statData,
+        type: 'horizontalBar',
+        options: {
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
