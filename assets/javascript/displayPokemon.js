b2fd3cfba06edefda2fd68350d0e782c1b1cf910
Kyle LaMont <kylerlamont@gmail.com>
2019-10-12 11:44:37 -0700

    added border to images and corrected image sizing to be proportional

--- assets/javascript/displayPokemon.js
+++ assets/javascript/displayPokemon.js
@@ -1,6 +1,6 @@
 function displayPokemon(pokemon){
     console.log(pokemon);
-    var pokimage = $("<img class=''>");
+    var pokimage = $("<img class='mx-auto border border-dark'>");
     pokimage.attr("src", pokemon.sprites.front_default);
     $("#sprite").empty();
     $("#sprite").prepend(pokimage);
