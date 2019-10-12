dee2259e9c031b405a36405bc2509d6e65a3605b
msheffield <msheffield@adaptivebiotech.com>
2019-10-12 12:51:55 -0700

    updatable stat progress bars

--- assets/javascript/displayPokemon.js
+++ assets/javascript/displayPokemon.js
@@ -1,4 +1,13 @@
-function displayPokemon(pokemon){
+function displayPokemon(pokemon) {
     console.log(pokemon);
     // add code to update UI elements here
+
+    pokemon.stats.forEach(element => {
+        let name = element.stat["name"];
+        let value = element["base_stat"];
+
+        $("#" + name).attr("aria-valuenow", value);
+        $("#" + name).attr("style", "width: " + ((parseInt(value) / 255) * 100) + "%");
+        $("#" + name).text(value);
+    })
 }
