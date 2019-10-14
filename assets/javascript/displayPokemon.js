6718005d72d7117fe23b81c63d7665ad30135dac
Max <maxthesheffield@gmail.com>
2019-10-13 21:19:33 -0700

    Google charts (#36)
    * bar graph added, known bug - multiple charts on multiple searches. Need to add .destroy somewhere
    
    * need to fix deleting old graph
    
    * saving
    
    * implemented google charts (instead of chart js), will make pretty tomorrow

--- assets/javascript/displayPokemon.js
+++ assets/javascript/displayPokemon.js
@@ -1,18 +1,42 @@
 function displayPokemon(pokemon) {
-    console.log(pokemon);
+    //console.log(pokemon);
     // add code to update UI elements here
 
-    pokemon.stats.forEach(element => {
-        let name = element.stat["name"];
-        let value = element["base_stat"];
-
-        $("#" + name).attr("aria-valuenow", value);
-        $("#" + name).attr("style", "width: " + ((parseInt(value) / 255) * 100) + "%");
-        $("#" + name).text(value);
-    })
+    updateStats(pokemon);
 
     var pokimage = $("<img class='mx-auto border border-dark'>");
     pokimage.attr("src", pokemon.sprites.front_default);
     $("#sprite").empty();
     $("#sprite").prepend(pokimage);
+}
+
+function updateStats(pokemon) {
+
+    google.charts.load('current', { 'packages': ['corechart'] });
+
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Stat');
+        data.addColumn('number', 'Value');
+
+        pokemon.stats.forEach(element => {
+            data.addRows([
+                [element.stat["name"], element["base_stat"]]
+            ]);
+        })
+
+        var options = {
+            'title': 'Statistics',
+            'width': 400,
+            'height': 300,
+            'legend': {position: 'none'}
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.BarChart(document.getElementById('chart'));
+        chart.draw(data, options);
+    }
 }
