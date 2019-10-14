4d41b7f1c63be95aa36ef77593096f8dd86056a6
Max <maxthesheffield@gmail.com>
2019-10-14 14:05:44 -0700

    added get pokemon names to doc ready function (#40)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    // Load pokemon names
+    getPokemonNames();
+
     // When submit button is clicked, send API call to pokeapi and giphy then clear the input value
     $("#btn-submit").on("click", function () {
         searchpokemon();
@@ -6,7 +9,7 @@ $(document).ready(function(){
     });
 
     // When a user begins to type in the input element, use autocomplete
-    $("#pokesearch").on("input",autocomplete);
+    $("#pokesearch").on("input", autocomplete);
 
     $(".balls").on("click", function() {
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
